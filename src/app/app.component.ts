9cd38925f298b488269c620e0aa26e6369d932a5
HanenAljadani <haneenabdullah66@hotmail.com>
2024-06-08 15:45:17 +0300

    update the content of the table and images

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,10 +9,10 @@ export class AppComponent {
   total:number=0;
   qty:number=0;
   foodmenu=[
-    {id:1,name:"دجاج",price:30,description:"دجاج مشوي على الفحم",img:"assets/images/checken.jpg"},
-    {id:2,name:"دجاج",price:30,description:"دجاج شواية ",img:"assets/images/checken.jpg"},
-    {id:3,name:"ايدام",price:20,description:"ايدام خضار مشكل",img:"assets/images/edam.jpg"},
-    {id:4,name:"ايدام",price:20,description:"ايدام  بالدجاج على الفرن",img:"assets/images/edam2.jpg"}
+    {id:1,name:"Chocolate Cake",price:30,description:"Rich, moist layers of dark chocolate with smooth ganache",img:"assets/images/choco.jpeg"},
+    {id:2,name:"Strawberry Cake",price:30,description:"Light sponge with fresh strawberries and creamy frosting",img:"assets/images/stra.jpeg"},
+    {id:3,name:"Cookie Cake",price:20,description:"Soft, chewy cookie in cake form with chocolate chips",img:"assets/images/coo.jpeg"},
+    {id:4,name:"Carrot Cake",price:20,description:"Moist and spiced with a creamy frosting",img:"assets/images/carrot.jpeg"}
   ]
   title = 'menu';
   sum(event:any, price:number){
